d6a93e04848232ed8659a07bcc1c46597999856d
miggy-pg <meelan1323@gmail.com>
2024-09-03 22:05:25 +0800

    wip: add styling in main-header

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -6,20 +6,22 @@ import React from "react";
 function MainHeader() {
   return (
     <header>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About Us</Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link href="/contact-us">Contact</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About Us</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/contact-us">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
