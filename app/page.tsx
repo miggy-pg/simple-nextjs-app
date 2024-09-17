f49da340e61633ee0742a99cc8fca0801c19f56a
miggy-pg <meelan1323@gmail.com>
2024-09-17 21:18:49 +0800

    feat: added menu

--- app/page.tsx
+++ app/page.tsx
@@ -40,9 +40,9 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <Link href="/about">About Us</Link>
-      </div>
+      </div> */}
     </main>
   );
 }
--- components/main-header.tsx
+++ components/main-header.tsx
@@ -15,9 +15,9 @@ import { buttonVariants } from "./ui/button";
 
 const MENU_ITEMS = [
   { name: "Home", icon: <Home />, link: "/" },
-  { name: "about", icon: <User2 />, link: "/about" },
-  { name: "projects", icon: <Layers />, link: "/projects" },
-  { name: "contact", icon: <Phone />, link: "/contact" },
+  { name: "About", icon: <User2 />, link: "/about" },
+  { name: "Projects", icon: <Layers />, link: "/projects" },
+  { name: "Contacts", icon: <Phone />, link: "/contact" },
 ];
 
 function MainHeader() {
@@ -25,27 +25,32 @@ function MainHeader() {
 
   return (
     <header>
-      <nav>
-        {MENU_ITEMS.map((item) => {
-          return (
-            <TooltipProvider key={item.name}>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <Link
-                    href={item.link}
-                    className={cn(
-                      buttonVariants({ variant: "ghost", size: "sm" }),
-                      "hover:text-[#2f7df4]",
-                      pathname === item.link && "text-[#2f7df4] bg-zinc-100"
-                    )}
-                  >
-                    {item.icon}
-                  </Link>
-                </TooltipTrigger>
-              </Tooltip>
-            </TooltipProvider>
-          );
-        })}
+      <nav className="flex flex-row justify-center">
+        <div className="fixed bottom-10 bg-[#3C3D37] rounded-3xl">
+          {MENU_ITEMS.map((item) => {
+            return (
+              <TooltipProvider key={item.name}>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <TooltipContent>
+                      <p>{item.name}</p>
+                    </TooltipContent>
+                    <Link
+                      href={item.link}
+                      className={cn(
+                        buttonVariants({ size: "xs" }),
+                        "hover:text-[#FADFA1] text-[#fff] mx-8",
+                        pathname === item.link && "text-[#FADFA1]"
+                      )}
+                    >
+                      <span>{item.icon}</span>
+                    </Link>
+                  </TooltipTrigger>
+                </Tooltip>
+              </TooltipProvider>
+            );
+          })}
+        </div>
       </nav>
     </header>
   );
