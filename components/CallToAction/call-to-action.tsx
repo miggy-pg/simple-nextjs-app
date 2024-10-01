d62a58194931b90f3ea037e1e7df679a6e769103
miggy-pg <meelan1323@gmail.com>
2024-10-01 22:27:55 +0800

    feat: added carousel in tabs

--- components/CallToAction/call-to-action.tsx
+++ components/CallToAction/call-to-action.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 function CallToAction() {
   return (
-    <section>
-      <Tabs defaultValue="services" className="w-[400px]">
+    <div>
+      <Tabs defaultValue="services" className="flex md:hidden w-[400px]">
         <TabsList>
-          <TabsTrigger value="services">Account</TabsTrigger>
-          <TabsTrigger value="offices">Password</TabsTrigger>
-          <TabsTrigger value="team">Password</TabsTrigger>
-          <TabsTrigger value="philosophy">Password</TabsTrigger>
-          <TabsTrigger value="spaces">Password</TabsTrigger>
+          <TabsTrigger value="services">Services</TabsTrigger>
+          <TabsTrigger value="offices">Offices</TabsTrigger>
+          <TabsTrigger value="team">Team</TabsTrigger>
+          <TabsTrigger value="philosophy">Philosophy</TabsTrigger>
+          <TabsTrigger value="spaces">Spaces</TabsTrigger>
         </TabsList>
-        <TabsContent value="services"></TabsContent>
+        <TabsContent value="services">
+          <Carousel>
+            <CarouselContent>
+              <CarouselItem>Test</CarouselItem>
+              <CarouselItem>...</CarouselItem>
+              <CarouselItem>...</CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        </TabsContent>
         <TabsContent value="offices"></TabsContent>
         <TabsContent value="team"></TabsContent>
         <TabsContent value="philosophy">Change your password here.</TabsContent>
         <TabsContent value="spaces">Change your password here.</TabsContent>
       </Tabs>
-    </section>
+    </div>
   );
 }
 
